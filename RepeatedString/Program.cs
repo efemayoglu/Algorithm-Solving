09fbfddce45c20abad8d4bf0bf36711c477a793c
Efe Mayoğlu <efemayoglu@hotmail.com>
2019-04-29 23:55:24 +0300

    4.problem Repeated string solved

---
+++ RepeatedString/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatedString
+{
+    class Program
+    {
+        static long repeatedString(string s, long n)
+        {
+            var aCountInS = counter(s);
+            var simpleAcount = (n / s.Length) * aCountInS;
+            for (int i = 0; i < n % s.Length; i++)
+                if (s[i] == 'a')
+                    simpleAcount++;
+
+            return simpleAcount;
+        }
+        static long counter(string s)
+        {
+            var first = s.IndexOf('a');
+            var counter = 0;
+            while (first != -1)
+            {
+                counter++;
+                first = s.IndexOf('a', first+1);
+            }
+            return counter;
+        }   
+
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            long result = repeatedString(s, n);
+            Console.WriteLine("res="+ result);
+            Console.ReadKey();
+        }
+    }
+}
+ 
